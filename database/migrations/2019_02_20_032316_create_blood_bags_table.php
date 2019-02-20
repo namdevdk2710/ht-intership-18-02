a7cd2b628afbd123596c4ce213a3c7cfcb85535f
ty <typhan0112@gmail.com>
2019-02-20 10:26:28 +0700

    Create-table-migration

---
+++ database/migrations/2019_02_20_032316_create_blood_bags_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBloodBagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blood_bags', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blood_bags');
+    }
+}
