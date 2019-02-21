3ec19d073c679622af062b84e7bfafc6c7e90e85
ndnhan132 <ndnhan132@gmail.com>
2019-02-21 09:48:44 +0700

    makeauth and create AdminMiddleware

---
+++ app/Http/Controllers/V1/Web/Backend/AdminController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\V1\Web\Backend;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AdminController extends Controller
+{
+    //
+}
