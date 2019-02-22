765a66beca2a123943ffbe90bb21d5387c9271fe
ndnhan132 <ndnhan132@gmail.com>
2019-02-22 10:40:03 +0700

    fix lg

--- app/Repositories/V1/User/UserRepository.php
+++ app/Repositories/V1/User/UserRepository.php
@@ -18,16 +18,19 @@ class UserRepository extends BaseRepository implements UserRepositoryInterface
         $user = User::where('email', $request->email)->first();
 
         if (! $user) {
-            return 0;
+            return 'email';
+        } elseif ($user->role !== 1) {
+            return 'role';
         }
         $data = [
             'email' => $request['email'],
             'password' => $request['password'],
+            'role' => 1,
         ];
         if (Auth::attempt($data)) {
-            return 2;
+            return 'password';
         }
 
-        return 1;
+        return 'success';
     }
 }
