fa7fb326b2aad8227dc6c851c11774e2bd0b9d04
ty <typhan0112@gmail.com>
2019-03-04 10:00:23 +0700

    modifile-migration-infor

--- database/migrations/2019_02_28_064250_create_information_table.php
+++ database/migrations/2019_02_28_064250_create_information_table.php
@@ -15,17 +15,17 @@ class CreateInformationTable extends Migration
     {
         Schema::create('information', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->boolean('gender');
-            $table->string('dob');
-            $table->string('cmnd');
-            $table->string('address');
-            $table->string('phone');
-            $table->integer('user_id')->unsigned();
+            $table->string('name')->nullable();
+            $table->integer('gender')->default(0);
+            $table->string('dob')->nullable();
+            $table->string('cmnd')->nullable();
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->integer('user_id')->unsigned()->nullable();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->integer('blood_id')->unsigned();
+            $table->integer('blood_id')->unsigned()->nullable();
             $table->foreign('blood_id')->references('id')->on('blood_groups')->onDelete('cascade');
-            $table->integer('commune_id')->unsigned();
+            $table->integer('commune_id')->unsigned()->nullable();
             $table->foreign('commune_id')->references('id')->on('communes')->onDelete('cascade');
             $table->timestamps();
         });
