f08a6f948d736ea70db5707f62264ffeb010ebe1
ty <typhan0112@gmail.com>
2019-03-04 09:30:00 +0700

    delete-user

--- resources/views/backend/users/index.blade.php
+++ resources/views/backend/users/index.blade.php
@@ -39,11 +39,12 @@
                         @endif
                     </td>
                     <td>
-                        <a class="btn btn-sm btn-primary" href="#" data-toggle="modal"
-                            data-target="#">Chi
+                        <a class="btn btn-sm btn-primary" href="#" data-toggle="modal" data-target="#">Chi
                             tiết</a>
                         <a class="btn btn-sm btn-warning" href="#">Sửa</a>
-                        <a class="btn btn-sm btn-danger" href="#">Xóa</a>
+                        <button type="button" value="{{$user->id}}" class="btn btn-sm btn-danger">
+                            Xóa
+                        </button>
                     </td>
                 </tr>
                 @endforeach
@@ -52,5 +53,18 @@
         </table>
     </div>
 </div>
+
+<script>
+    $(document).ready(function() {
+        var button = $('.btn-danger');
+        button.click(function() {
+            if (confirm("Bạn có muốn xóa người dùng này?")) {
+                var url = "{{ route('users.destroy', ':id') }}";
+                url = url.replace(':id', $(this).val());
+                window.location.href = url;
+            }
+        });
+    });
+</script>
 
 @endsection
