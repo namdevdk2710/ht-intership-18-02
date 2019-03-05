7563fe5f4a7a078cf970d139c117dd2c7aa49b09
ty <typhan0112@gmail.com>
2019-03-05 17:10:51 +0700

    show-calendar

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,10 +7,5 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
-    }
 }
