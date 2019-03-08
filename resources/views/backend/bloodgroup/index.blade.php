2b714f9ba0fc055e590e302481ceadfe4709f2db
ty <typhan0112@gmail.com>
2019-03-08 09:49:59 +0700

    fix-bloods

--- resources/views/backend/bloodgroup/index.blade.php
+++ resources/views/backend/bloodgroup/index.blade.php
@@ -11,27 +11,27 @@
     <table class="table table-hover">
         <thead>
             <tr>
-                <th scope="col">STT</th>
-                <th scope="col">Tên nhóm</th>
-                <th scope="col">Hoạt động</th>
+                <th class="text-center" scope="col">STT</th>
+                <th class="text-center" scope="col">Tên nhóm</th>
+                <th class="text-center" scope="col">Hoạt động</th>
             </tr>
         </thead>
         <tbody>
             @foreach($bloods as $key => $blood)
             <tr>
-                <th scope="row">{{ $key +1 }}</th>
-                <td>{{ $blood->name }}</td>
-                <td>
+                <td class="text-center">{{ $key +1 }}</td>
+                <td class="text-center">{{ $blood->name }}</td>
+                <td class="text-center">
                     <a class="btn btn-sm btn-warning" data-toggle="modal" data-target="#edit-blood{{$blood->id}}"
                         type="button">
                         Sửa
                     </a>
-                    @include('backend.bloodgroup.edit')
                     <button type="button" value="{{$blood->id}}" class="btn btn-sm btn-danger">
                         Xóa
                     </button>
                 </td>
             </tr>
+            @include('backend.bloodgroup.edit')
             @endforeach
         </tbody>
     </table>
@@ -41,16 +41,16 @@
 </div>
 
 <script>
-    $(document).ready(function() {
-        var button = $('.btn-danger');
-        button.click(function() {
-            if (confirm("Bạn có muốn xóa nhóm máu này?")) {
-                var url = '{{ route("bloods.destroy", ":id") }}';
-                url = url.replace(':id', $(this).val());
-                window.location.href = url;
-            }
-        });
+$(document).ready(function() {
+    var button = $('.btn-danger');
+    button.click(function() {
+        if (confirm("Bạn có muốn xóa nhóm máu này?")) {
+            var url = '{{ route("bloods.destroy", ":id") }}';
+            url = url.replace(':id', $(this).val());
+            window.location.href = url;
+        }
     });
+});
 </script>
 
 @endsection
