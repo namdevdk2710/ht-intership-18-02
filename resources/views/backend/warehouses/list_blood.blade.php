9c2ef168c91528baafa6f60a003e789e2262cc23
ty <typhan0112@gmail.com>
2019-03-14 21:13:03 +0700

    list-rêciver

---
+++ resources/views/backend/warehouses/list_blood.blade.php
@@ -0,0 +1,21 @@
+<div class="title-request">
+    <h4>Danh sách các túi máu đạt yêu cầu</h4>
+</div>
+<table class="table table-hover">
+    <thead>
+        <tr class="text-center">
+            <th scope="col">STT</th>
+            <th scope="col">Mã túi máu</th>
+            <th scope="col">Kho</th>
+            <th scope="col">Nhóm máu</th>
+            <th scope="col">Thể tích</th>
+            <th scope="col">Ngày hết hạn</th>
+            <th scope="col">Thao tác</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
