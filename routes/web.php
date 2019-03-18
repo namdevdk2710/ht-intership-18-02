ebdfc3f3981e1d639d9c84ee56ca42a0ca76428c
ty <typhan0112@gmail.com>
2019-03-18 15:52:11 +0700

    fix file web.php

--- routes/web.php
+++ routes/web.php
@@ -1,90 +1,96 @@
 <?php
-Route::group(['prefix' => '/'], function () {
-    Route::get('', 'V1\Web\Frontend\PageController@index')->name('home');
-    Route::post('/login', 'V1\Web\Frontend\PageController@postLogin')->name('postLogin');
-    Route::post('/register', 'V1\Web\Frontend\PageController@postRegister')->name('postRegister');
-    Route::get('/logout', 'V1\Web\Frontend\PageController@logout')->name('logout');
+// ---------------------------Front-end routes-----------------------------//
+Route::group(['prefix' => '/', 'namespace' => 'V1\Web\Frontend'], function () {
+    Route::get('', 'PageController@index')->name('home');
+    Route::post('/login', 'PageController@postLogin')->name('postLogin');
+    Route::post('/register', 'PageController@postRegister')->name('postRegister');
+    Route::get('/logout', 'PageController@logout')->name('logout');
 });
-
+//---------------------------Admin-Login----------------------------------------//
 Route::get('admin/login', 'V1\Web\Backend\AdminController@getLogin')->name('admin.getLogin');
 Route::post('admin/login', 'V1\Web\Backend\AdminController@postLogin')->name('admin.postLogin');
 
-Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function () {
-    Route::get('/', 'V1\Web\Backend\AdminController@index')->name('admin.index');
-    Route::get('/logout', 'V1\Web\Backend\AdminController@logout')->name('admin.logout');
-
+//---------------------------Back-end routes----------------------------------------//
+Route::group(['prefix' => 'admin', 'namespace' => 'V1\Web\Backend', 'middleware' => 'admin'], function () {
+//---------------------------Admin index----------------------------------------//
+    Route::get('/', 'AdminController@index')->name('admin.index');
+//---------------------------Admin-logout----------------------------------------//
+    Route::get('/logout', 'AdminController@logout')->name('admin.logout');
+//---------------------------Admin-profile----------------------------------------//
     Route::group(['prefix' => 'profile'], function () {
-        Route::get('/', 'V1\Web\Backend\UserController@profile')->name('user.profile');
-        Route::post('/', 'V1\Web\Backend\UserController@changeAdminPassword')->name('user.changeAdminPassword');
+        Route::get('/', 'UserController@profile')->name('user.profile');
+        Route::post('/', 'UserController@changeAdminPassword')->name('user.changeAdminPassword');
     });
-
+//---------------------------Admin-calendar----------------------------------------//
     Route::group(['prefix' => 'calendar'], function () {
-        Route::get('/', 'V1\Web\Backend\CalendarController@index')->name('calendar.index');
-        Route::post('/add-new', 'V1\Web\Backend\CalendarController@postAddCalendar')->name('calendar.postAddCalendar');
-        Route::post('/showDistrictInCity', 'V1\Web\Backend\CalendarController@showDistrictInCity')
+        Route::get('/', 'CalendarController@index')->name('calendar.index');
+        Route::post('/add-new', 'CalendarController@postAddCalendar')->name('calendar.postAddCalendar');
+        Route::post('/showDistrictInCity', 'CalendarController@showDistrictInCity')
         ->name('calendar.showDistrictInCity');
-        Route::post('/showCommuneInDistrict', 'V1\Web\Backend\CalendarController@showCommuneInDistrict')
+        Route::post('/showCommuneInDistrict', 'CalendarController@showCommuneInDistrict')
         ->name('calendar.showCommuneInDistrict');
-        Route::delete('/delete/{id}', 'V1\Web\Backend\CalendarController@delete')->name('calendar.delete');
-        Route::put('/edit/{id}', 'V1\Web\Backend\CalendarController@update')->name('calendar.update');
-        Route::get('/{id}', 'V1\Web\Backend\CalendarController@show')->name('calendar.show');
+        Route::delete('/delete/{id}', 'CalendarController@delete')->name('calendar.delete');
+        Route::put('/edit/{id}', 'CalendarController@update')->name('calendar.update');
+        Route::get('/{id}', 'CalendarController@show')->name('calendar.show');
     });
-
+//---------------------------Admin-Request-Blood----------------------------------------//
     Route::group(['prefix' => 'request-bloods'], function () {
-        Route::get('/donated', 'V1\Web\Backend\RequestBloodController@donated')->name('request-bloods.donated');
-        Route::get('/received', 'V1\Web\Backend\RequestBloodController@received')->name('request-bloods.received');
-        Route::get('/donated/confirm/{id}', 'V1\Web\Backend\RequestBloodController@confirm')
+        Route::get('/donated', 'RequestBloodController@donated')->name('request-bloods.donated');
+        Route::get('/received', 'RequestBloodController@received')->name('request-bloods.received');
+        Route::get('/donated/confirm/{id}', 'RequestBloodController@confirm')
         ->name('request-bloods.confirm');
-        Route::get('/received/confirm/{id}', 'V1\Web\Backend\RequestBloodController@confirm')
+        Route::get('/received/confirm/{id}', 'RequestBloodController@confirm')
         ->name('request-bloods.confirm');
     });
-
+//---------------------------Admin-Blood-Bag----------------------------------------//
     Route::group(['prefix' => 'blood-bags'], function () {
-        Route::get('/import', 'V1\Web\Backend\BloodBagController@getImport')->name('blood-bags.getImport');
-        Route::Post('/getInfoByCode', 'V1\Web\Backend\BloodBagController@getInfoByCode');
-        Route::post('/store', 'V1\Web\Backend\BloodBagController@store')->name('blood-bags.store');
-        Route::get('/search', 'V1\Web\Backend\BloodBagController@getSearch')->name('blood-bags.getSearch');
-        Route::get('/search-blood-bag', 'V1\Web\Backend\BloodBagController@searchBloodBagByCode');
+        Route::get('/import', 'BloodBagController@getImport')->name('blood-bags.getImport');
+        Route::Post('/getInfoByCode', 'BloodBagController@getInfoByCode');
+        Route::post('/store', 'BloodBagController@store')->name('blood-bags.store');
+        Route::get('/search', 'BloodBagController@getSearch')->name('blood-bags.getSearch');
+        Route::get('/search-blood-bag', 'BloodBagController@searchBloodBagByCode');
     });
-
-    Route::group(['prefix' => 'hourseware'], function () {
-        Route::get('/', 'V1\Web\Backend\WareHouseController@index')->name('warehouses.index');
-        Route::post('/add', 'V1\Web\Backend\WareHouseController@store')->name('warehouses.store');
-        Route::put('/edit/{id}', 'V1\Web\Backend\WareHouseController@update')->name('warehouses.update');
-        Route::get('/delete/{id}', 'V1\Web\Backend\WareHouseController@destroy')->name('warehouses.destroy');
-        Route::get('/export-blood', 'V1\Web\Backend\WareHouseController@getExport')->name('export-bloods.index');
-        Route::get('/export-blood/{id}', 'V1\Web\Backend\WareHouseController@getExportRequest')
-            ->name('export-request');
-        Route::get('/export-blood/{id}/{id2}', 'V1\Web\Backend\WareHouseController@confirm')
-            ->name('confirm-request');
-        Route::get('/import-blood', 'V1\Web\Backend\WareHouseController@getImport')->name('import-loods.index');
-        Route::post('/import-blood/{id}', 'V1\Web\Backend\WareHouseController@import')->name('import-loods');
-        Route::get('/blood-bag', 'V1\Web\Backend\WareHouseController@listBloodBag')->name('blood-bags.index');
-        Route::get('/blood-bag/{id}', 'V1\Web\Backend\WareHouseController@detailBloodBag')
+//---------------------------------ADMIN WAREHOUSE----------------------------------------------//
+    Route::group(['prefix' => 'warehouses'], function () {
+        //---------------------------WareHouse-Manager----------------------------------------//
+        Route::get('/', 'WareHouseController@index')->name('warehouses.index');
+        Route::post('/add', 'WareHouseController@store')->name('warehouses.store');
+        Route::put('/edit/{id}', 'WareHouseController@update')->name('warehouses.update');
+        Route::get('/delete/{id}', 'WareHouseController@destroy')->name('warehouses.destroy');
+        //---------------------------Export-BloodBag----------------------------------------//
+        Route::get('/export-blood', 'WareHouseController@getExport')->name('export-bloods.index');
+        Route::get('/export-blood/{id}', 'WareHouseController@getExportRequest')->name('export-request');
+        Route::get('/export-blood/{id}/{id2}', 'WareHouseController@confirm')->name('confirm-request');
+        //---------------------Import-BloodBag-into-WareHouse---------------------------------//
+        Route::get('/import-blood', 'WareHouseController@getImport')->name('import-loods.index');
+        Route::post('/import-blood/{id}', 'WareHouseController@import')->name('import-loods');
+        //-----------------------Manager-BloodBag-In-WareHouse------------------------------------//
+        Route::get('/blood-bag', 'WareHouseController@listBloodBag')->name('blood-bags.index');
+        Route::get('/blood-bag/{id}', 'WareHouseController@detailBloodBag')
         ->name('blood-bags.updateStatus');
-        Route::post('/blood-bag/{id}', 'V1\Web\Backend\WareHouseController@updateStatus')->name('updateStatus');
+        Route::post('/blood-bag/{id}', 'WareHouseController@updateStatus')->name('updateStatus');
     });
-
+//-----------------------------------DIARY---------------------------------------//
     Route::group(['prefix' => 'diary'], function () {
-        Route::get('/', 'V1\Web\Backend\DiaryController@index')->name('diary.index');
-        Route::post('/', 'V1\Web\Backend\DiaryController@search')->name('diary.search');
+        Route::get('/', 'DiaryController@index')->name('diary.index');
+        Route::post('/', 'DiaryController@search')->name('diary.search');
     });
-});
-
-Route::group(['prefix' => 'users'], function () {
-    Route::get('/', 'V1\Web\Backend\UserController@index')->name('users.list');
-    Route::get('/create', 'V1\Web\Backend\UserController@create');
-    Route::post('/create', 'V1\Web\Backend\UserController@store')->name('users.store');
-    Route::get('edit/{id}', 'V1\Web\Backend\UserController@edit');
-    Route::put('edit/{id}', 'V1\Web\Backend\UserController@update')->name('users.update');
-    Route::get('delete/{id}', 'V1\Web\Backend\UserController@destroy')->name('users.destroy');
-});
-
-Route::group(['prefix' => 'bloods'], function () {
-    Route::get('', 'V1\Web\Backend\BloodController@index')->name('bloods.list');
-    Route::get('create', 'V1\Web\Backend\BloodController@create');
-    Route::post('create', 'V1\Web\Backend\BloodController@store')->name('bloods.store');
-    Route::get('edit/{id}', 'V1\Web\Backend\BloodController@edit');
-    Route::put('edit/{id}', 'V1\Web\Backend\BloodController@update')->name('bloods.update');
-    Route::get('delete/{id}', 'V1\Web\Backend\BloodController@destroy')->name('bloods.destroy');
+//-----------------------------------USER-MANAGER---------------------------------------//
+    Route::group(['prefix' => 'users'], function () {
+        Route::get('/', 'UserController@index')->name('users.list');
+        Route::get('/create', 'UserController@create');
+        Route::post('/create', 'UserController@store')->name('users.store');
+        Route::get('edit/{id}', 'UserController@edit');
+        Route::put('edit/{id}', 'UserController@update')->name('users.update');
+        Route::get('delete/{id}', 'UserController@destroy')->name('users.destroy');
+    });
+//-----------------------------------BLOODGROUP-MANAGER---------------------------------------//
+    Route::group(['prefix' => 'bloods'], function () {
+        Route::get('', 'BloodController@index')->name('bloods.list');
+        Route::get('create', 'BloodController@create');
+        Route::post('create', 'BloodController@store')->name('bloods.store');
+        Route::get('edit/{id}', 'BloodController@edit');
+        Route::put('edit/{id}', 'BloodController@update')->name('bloods.update');
+        Route::get('delete/{id}', 'BloodController@destroy')->name('bloods.destroy');
+    });
 });
