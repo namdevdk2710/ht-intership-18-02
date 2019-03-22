1475aab349ffe0e96489f9b640319cb4ac8ad766
ty <typhan0112@gmail.com>
2019-03-22 11:09:37 +0700

    home-page

---
+++ resources/views/frontend/pages/slogan.blade.php
@@ -0,0 +1,12 @@
+<div class="admin p-4 my-4 text-center">
+    <p>
+        <i class="fas fa-quote-left"></i>
+        HÃY NGHĨ ĐẾN NGƯỜI BỆNH -
+        SỰ SỐNG VÀ CÁI CHẾT ĐANG ĐE DỌA HỌ,
+        BẰNG CÁCH HIẾN MÁU,
+        BẠN CÓ THỂ TẠO SỰ KHÁC BIỆT
+        GIỮA SỰ SỐNG VÀ CÁI CHẾT
+        <i class="fas fa-quote-right"></i>
+    </p>
+    <a href="#" class="mt-3 font-weight-bold text-right "> --> Thông điệp hiến máu <--</a>
+</div>
---
+++ resources/views/frontend/posts/index.blade.php
@@ -0,0 +1,131 @@
+<div class="row single-left2">
+    <div class="col-md-6 single-left2-left">
+        <ul>
+            <li class="mt-3">
+                <i class="fas fa-check mr-2"></i>
+                <a href="single.html">At vero eos et accusamus iusto</a>
+            </li>
+            <li class="mt-3">
+                <i class="fas fa-check mr-2"></i>
+                <a href="single.html">Sed ut perspiciatis unde omnis</a>
+            </li>
+            <li class="mt-3">
+                <i class="fas fa-check mr-2"></i>
+                <a href="single.html">Accusantium doloremque lauda</a>
+            </li>
+            <li class="mt-3">
+                <i class="fas fa-check mr-2"></i>
+                <a href="single.html">Vel illum qui dolorem fugiat quo</a>
+            </li>
+            <li class="mt-3">
+                <i class="fas fa-check mr-2"></i>
+                <a href="single.html">Quis autem vel eum repreh</a>
+            </li>
+            <li class="mt-3">
+                <i class="fas fa-check mr-2"></i>
+                <a href="single.html">Neque porro quisquam est qui</a>
+            </li>
+        </ul>
+    </div>
+    <div class="col-md-6 single-left2-left">
+        <ul>
+            <li class="mt-3">
+                <i class="fas fa-check mr-2"></i>
+                <a href="single.html">At vero eos et accusamus iusto</a>
+            </li>
+            <li class="mt-3">
+                <i class="fas fa-check mr-2"></i>
+                <a href="single.html">Sed ut perspiciatis unde omnis</a>
+            </li>
+            <li class="mt-3">
+                <i class="fas fa-check mr-2"></i>
+                <a href="single.html">Accusantium doloremque lauda</a>
+            </li>
+            <li class="mt-3">
+                <i class="fas fa-check mr-2"></i>
+                <a href="single.html">Vel illum qui dolorem fugiat quo</a>
+            </li>
+            <li class="mt-3">
+                <i class="fas fa-check mr-2"></i>
+                <a href="single.html">Quis autem vel eum repreh</a>
+            </li>
+            <li class="mt-3">
+                <i class="fas fa-check mr-2"></i>
+                <a href="single.html">Neque porro quisquam est qui</a>
+            </li>
+        </ul>
+    </div>
+</div>
+<div class="comments my-5">
+    <h3 class="blog-title text-dark">Our Recent Comments</h3>
+    <div class="comments-grids mt-4">
+        <div class="row comments-grid">
+            <div class="col-3 comments-grid-left">
+                <img src="images/te1.jpg" alt=" " class="img-thumbnail" />
+            </div>
+            <div class="col-8 comments-grid-right mt-3">
+                <h4>Parker Moe</h4>
+                <ul class="my-2">
+                    <li class="font-weight-bold">6 June 2018
+                        <i>|</i>
+                    </li>
+                    <li>
+                        <a href="#" class="font-weight-bold">Reply</a>
+                    </li>
+                </ul>
+                <p>mattis ut hendrerit non, facilisis eget mauris. Sed ultricies nec purus quis
+                    tempor. Phasellus bibendum
+                    eu.</p>
+            </div>
+        </div>
+        <div class="row comments-grid my-4">
+            <div class="col-3 comments-grid-left">
+                <img src="images/te2.jpg" alt=" " class="img-thumbnail" />
+            </div>
+            <div class="col-8 comments-grid-right mt-3">
+                <h4>Mary Jane</h4>
+                <ul class="my-2">
+                    <li class="font-weight-bold">16 May 2018
+                        <i>|</i>
+                    </li>
+                    <li>
+                        <a href="#" class="font-weight-bold">Reply</a>
+                    </li>
+                </ul>
+                <p>mattis ut hendrerit non, facilisis eget mauris. Sed ultricies nec purus quis
+                    tempor. Phasellus bibendum
+                    eu.</p>
+            </div>
+        </div>
+        <div class="row comments-grid">
+            <div class="col-3 comments-grid-left">
+                <img src="images/te3.jpg" alt=" " class="img-thumbnail" />
+            </div>
+            <div class="col-8 comments-grid-right mt-3">
+                <h4>Michael Loe</h4>
+                <ul class="my-2">
+                    <li class="font-weight-bold">16 June 2018
+                        <i>|</i>
+                    </li>
+                    <li>
+                        <a href="#" class="font-weight-bold">Reply</a>
+                    </li>
+                </ul>
+                <p>mattis ut hendrerit non, facilisis eget mauris. Sed ultricies nec purus quis
+                    tempor. Phasellus bibendum
+                    eu.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="leave-coment-form">
+    <h3 class="blog-title text-dark mb-4">Leave a Reply</h3>
+    <form action="#" method="post">
+        <input type="text" name="Name" placeholder="Name" required="">
+        <input type="email" name="Email" placeholder="Email" required="">
+        <textarea name="Message" placeholder="Your comment here..." required=""></textarea>
+        <div class="mm_single_submit">
+            <input type="submit" value="Post Comment">
+        </div>
+    </form>
+</div>
